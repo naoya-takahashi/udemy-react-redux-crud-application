094f1926ae3d0568dd29c7535050f10088f7839c
naoya_takahashi <49216915+naoya-takahashi@users.noreply.github.com>
2020-09-17 11:28:06 +0900

    jsxについて

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 function App() {
+  // const greeting = 'Hi Tom!';
+  // const dom = <h1 className='foo'>{greeting}</h1>
+  // return dom;
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <React.Fragment>
+      <label htmlFor='bar'>bar</label>
+      <input type='text' onClick={() => {console.log('im am clicked')}}></input>
+    </React.Fragment>
   );
 }
 
