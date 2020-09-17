af1d5ee0cf19cf5803f1506b6f71f00a617ec01b
naoya_takahashi <49216915+naoya-takahashi@users.noreply.github.com>
2020-09-17 12:09:01 +0900

    propsの作成

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-/* 関数(function)コンポーネント */
-// function App() {
 const App = () =>{
+  const profiles = [
+    {name: 'taro', age: 10},
+    {name: 'chie', age: 16},
+    {name: 'hideki'},
+]
   return (
     <React.Fragment>
-      <label htmlFor='bar'>bar</label>
-      <input type='text' onClick={() => {console.log('im am clicked')}}></input>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </React.Fragment>
   );
 }
 
-// /* クラス(class)コンポーネント */
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor='bar'>bar</label>
-//         <input type='text' onClick={() => {console.log('im am clicked')}}></input>
-//       </React.Fragment>
-//     )
-//   }
-// }
+const User = (props) => {
+return <div>Hi I am {props.name} and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
+}
 
 export default App;
