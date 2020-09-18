dbc07aa842a7e42076951f10ced16319fed5ab3b
naoya_takahashi <49216915+naoya-takahashi@users.noreply.github.com>
2020-09-18 13:27:54 +0900

    events_newの作成

---
+++ src/components/events_new.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+// import { postEvents } from '../actions';
+
+class EventsNew extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        実装予定
+      </React.Fragment>
+    )
+  }
+}
+
+// const mapDispatchToProps = ({ hostEvents });
+
+export default connect(null, null)(EventsNew);
